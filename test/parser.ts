95bc2f42aae21be7787ea2b3fe7487b267dcd976
Ziphil <yfe23457@nifty.com>
2021-11-29 04:44:32 +0900

    コメントのパースのテストを追加

--- test/parser.ts
+++ test/parser.ts
@@ -6,9 +6,11 @@ import {
 import {
   BaseZenmlParser
 } from "../source";
+import {
+  dedent
+} from "./util";
 
 
-let raw = String.raw;
 let serializer = new XMLSerializer();
 
 function compare(input: string, output: string): void {
@@ -16,12 +18,87 @@ function compare(input: string, output: string): void {
   expect(serializer.serializeToString(parser.tryParse(input))).toBe(output);
 }
 
-describe("minimal", () => {
-  test("minimal", () => {
-    compare(raw`texttext`, raw`texttext`);
-    compare(raw`\element<text>`, raw`<element>text</element>`);
-    compare(raw`\element||<no attr>`, raw`<element>no attr</element>`);
-    compare(raw`\element|attr="val"|<text>`, raw`<element attr="val">text</element>`);
-    compare(raw`\element|attr="val",foo="bar",neko="usa"|<text>`, raw`<element attr="val" foo="bar" neko="usa">text</element>`);
+describe("elements and texts", () => {
+  test("basic", () => {
+    compare(dedent`texttext`, dedent`texttext`);
+    compare(dedent`\element<text>`, dedent`<element>text</element>`);
+  });
+  test("with attributes", () => {
+    compare(dedent`\element|attr="val"|<text>`, dedent`<element attr="val">text</element>`);
+    compare(dedent`\element|attr="val",foo="bar",neko="mofu"|<text>`, dedent`<element attr="val" foo="bar" neko="mofu">text</element>`);
+  });
+  test("no attributes", () => {
+    compare(dedent`\element||<no attr>`, dedent`<element>no attr</element>`);
+  });
+  test("boolean attributes", () => {
+    compare(dedent`\element|bool|<text>`, dedent`<element bool="bool">text</element>`);
+    compare(dedent`\element|bool,another|<text>`, dedent`<element bool="bool" another="another">text</element>`);
+  });
+  test("complex", () => {
+    compare(dedent`\nest<text\nest<inner>text>`, dedent`<nest>text<nest>inner</nest>text</nest>`);
+    compare(dedent`
+      \foo<\bar<\baz<neko>>>outer\foo<\bar<neko>>
+      \foo<
+        neko\bar<mofu>
+        neko
+      >
+    `, dedent`
+      <foo><bar><baz>neko</baz></bar></foo>outer<foo><bar>neko</bar></foo>
+      <foo>
+        neko<bar>mofu</bar>
+        neko
+      </foo>
+    `);
+  });
+});
+
+describe("block comments", () => {
+  test("basic", () => {
+    compare(dedent`#<comment>`, dedent`<!--comment-->`);
+    compare(dedent`#<comment>outer#<another>`, dedent`<!--comment-->outer<!--another-->`);
+    compare(dedent`\foo<#<comment>outer>`, dedent`<foo><!--comment-->outer</foo>`);
+  });
+  test("multiline", () => {
+    compare(dedent`
+      #<
+        multiline block comment
+        second line
+      >
+    `, dedent`
+      <!--
+        multiline block comment
+        second line
+      -->
+    `);
+  });
+});
+
+describe("line comments", () => {
+  test("basic", () => {
+    compare(dedent`
+      text
+      ##line comment
+      text
+    `, dedent`
+      text
+      <!--line comment-->text
+    `);
+    compare(dedent`
+      \foo<
+        ##\foo<>
+      >
+    `, dedent`
+      <foo>
+        <!--\foo<>--></foo>
+    `);
+  });
+  test("ending at eof", () => {
+    compare(dedent`
+      text
+      ##line comment ends at eof
+    `, dedent`
+      text
+      <!--line comment ends at eof-->
+    `);
   });
 });
---
+++ test/util.ts
@@ -0,0 +1,31 @@
+//
+
+
+export function dedent(templateStrings: TemplateStringsArray, ...values: Array<unknown>): string {
+  let strings = [...templateStrings.raw];
+  strings[strings.length - 1] = strings[strings.length - 1].replace(/\r?\n([\t ]*)$/, "");
+  let indentLengths = strings.reduce<Array<number>>((previous, string) => {
+    let match = string.match(/\n([\t ]+|(?!\s).)/g);
+    if (match) {
+      return previous.concat(match.map((part) => part.match(/[\t ]/g)?.length ?? 0));
+    } else {
+      return previous;
+    }
+  }, []);
+  if (indentLengths.length) {
+    let pattern = new RegExp(`\n[\t ]{${Math.min(...indentLengths)}}`, "g");
+    strings = strings.map((string) => string.replace(pattern, "\n"));
+  }
+  let string = strings[0];
+  string = string.replace(/^\r?\n/, "");
+  values.forEach((value, i) => {
+    let endentations = string.match(/(?:^|\n)( *)$/);
+    let endentation = (endentations) ? endentations[1] : "";
+    let indentedValue = value;
+    if (typeof value === "string" && value.includes("\n")) {
+      indentedValue = String(value).split("\n").map((string, i) => (i === 0) ? string : `${endentation}${string}`).join("\n");
+    }
+    string += indentedValue + strings[i + 1];
+  });
+  return string;
+}
