61420104562a967e014e59aad5f756587abfa0a1
Ziphil <yfe23457@nifty.com>
2021-11-29 04:52:21 +0900

    不要な処理を削除

--- test/util.ts
+++ test/util.ts
@@ -16,18 +16,8 @@ export function dedent(templateStrings: TemplateStringsArray, ...values: Array<u
     let pattern = new RegExp(`\n[\t ]{${Math.min(...indentLengths)}}`, "g");
     strings = strings.map((string) => string.replace(pattern, "\n"));
   }
-  let string = strings[0];
-  string = string.replace(/^\r?\n/, "");
-  values.forEach((value, i) => {
-    let endentations = string.match(/(?:^|\n)( *)$/);
-    let endentation = (endentations) ? endentations[1] : "";
-    let indentedValue = value;
-    if (typeof value === "string" && value.includes("\n")) {
-      indentedValue = String(value).split("\n").map((string, i) => (i === 0) ? string : `${endentation}${string}`).join("\n");
-    }
-    string += indentedValue + strings[i + 1];
-  });
-  return string;
+  let output = strings.join("").replace(/^\r?\n/, "");
+  return output;
 }
 
 export let $ = dedent;
