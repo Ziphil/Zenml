9e54a82eb3936c782e2ae74f89cb10bcafe13938
Ziphil <yfe23457@nifty.com>
2021-11-30 17:51:30 +0900

    変更

--- source/dom/constructor.ts
+++ source/dom/constructor.ts
@@ -11,7 +11,7 @@ let element = document.createElement("dummy");
 let text = document.createTextNode("dummy");
 let comment = document.createComment("dummy");
 
-global.Node = require("@xmldom/xmldom/lib/dom").Node;
-global.Element = element.constructor as any;
-global.Text = text.constructor as any;
-global.Comment = comment.constructor as any;
+global.Node ??= require("@xmldom/xmldom/lib/dom").Node;
+global.Element ??= element.constructor as any;
+global.Text ??= text.constructor as any;
+global.Comment ??= comment.constructor as any;
