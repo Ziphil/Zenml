11f3dd3e07dbdccf882ead4a56d90943eecb3d17
Ziphil <yfe23457@nifty.com>
2021-12-01 01:19:14 +0900

    実践的なテストケースを追加

---
+++ test/parser/practical.ts
@@ -0,0 +1,26 @@
+//
+
+import fs from "fs";
+import {
+  ZenmlParserOptions,
+  ZenmlPlugin
+} from "../../source";
+import {
+  shouldEquivalent
+} from "./util";
+
+
+function shouldEquivalentFile(name: string, options?: ZenmlParserOptions, plugins?: Array<[string, ZenmlPlugin]>): void {
+  let input = fs.readFileSync(`./test/parser/file/${name}.zml`, {encoding: "utf-8"});
+  let output = fs.readFileSync(`./test/parser/file/${name}.xml`, {encoding: "utf-8"});
+  shouldEquivalent(input, output, options, plugins);
+}
+
+describe("practical examples", () => {
+  test("diary", () => {
+    let options = {specialElementNames: {brace: "x", bracket: "xn", slash: "i"}};
+    shouldEquivalentFile("diary", options);
+  });
+  test("page", () => {
+  });
+});
