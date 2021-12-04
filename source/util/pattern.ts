59f6fc0339401b97ba5cf563ebc9175f8ae1efc8
Ziphil <yfe23457@nifty.com>
2021-12-04 18:03:08 +0900

    文字列のマッチパターンに配列を許すよう変更

--- source/util/pattern.ts
+++ source/util/pattern.ts
@@ -2,15 +2,17 @@
 
 
 export function matchString(string: string, pattern: StringPattern): boolean {
-  if (typeof pattern === "function") {
-    return pattern(string);
-  } else if (pattern instanceof RegExp) {
-    return string.match(pattern) !== null;
+  if (typeof pattern === "string") {
+    return string === pattern;
   } else if (typeof pattern === "boolean") {
     return pattern;
+  } else if (pattern instanceof RegExp) {
+    return string.match(pattern) !== null;
+  } else if (typeof pattern === "function") {
+    return pattern(string);
   } else {
-    return string === pattern;
+    return pattern.every((eachPattern) => matchString(string, eachPattern));
   }
 }
 
-export type StringPattern = string | boolean | RegExp | ((string: string) => boolean);
+export type StringPattern = string | boolean | RegExp | ((string: string) => boolean) | Array<StringPattern>;
